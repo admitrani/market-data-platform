f8433ed67a15975a22ccfe26d67d6c517871711b
Adam Mitrani <adammmitrani@gmail.com>
2026-05-18 11:04:10 +0200

    Make fact prices mart incremental

--- dbt/models/marts/fact_prices.sql
+++ dbt/models/marts/fact_prices.sql
@@ -1,32 +1,49 @@
 {{
     config(
-        materialized="table",
+        materialized="incremental",
+        incremental_strategy="merge",
+        unique_key=["symbol", "bar_interval", "open_time_utc"],
         partition_by={
             "field": "open_time_utc",
             "data_type": "timestamp",
             "granularity": "day"
         },
-        cluster_by=["symbol", "bar_interval"]
+        cluster_by=["symbol", "bar_interval"],
+        on_schema_change="fail"
     )
 }}
 
-select
-    symbol,
-    bar_interval,
-    open_time_utc,
-    open_date,
+with source_data as (
 
-    open,
-    high,
-    low,
-    close,
-    volume,
+    select
+        symbol,
+        bar_interval,
+        open_time_utc,
+        open_date,
 
-    quote_asset_volume,
-    number_of_trades,
-    taker_buy_base_asset_volume,
-    taker_buy_quote_asset_volume,
+        open,
+        high,
+        low,
+        close,
+        volume,
 
-    loaded_at,
-    batch_id
-from {{ ref("int_klines_normalized") }}
+        quote_asset_volume,
+        number_of_trades,
+        taker_buy_base_asset_volume,
+        taker_buy_quote_asset_volume,
+
+        loaded_at,
+        batch_id
+    from {{ ref("int_klines_normalized") }}
+
+    {% if is_incremental() %}
+        where open_time_utc >= (
+            select coalesce(max(open_time_utc), timestamp('1900-01-01'))
+            from {{ this }}
+        )
+    {% endif %}
+
+)
+
+select *
+from source_data
